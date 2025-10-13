ee0880a584ec63b250f160f4583cbfdefe0b401d
neriidev <rodrigogregorioneri2014@gmail.com>
2025-10-13 14:48:41 -0300

    criando controllers seguindo API First

---
+++ pix-backend/src/main/java/com/neriidev/pix/infrastructure/in/controller/PixController.java
@@ -0,0 +1,9 @@
+package com.neriidev.pix.infrastructure.in.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pix")
+public class PixController {
+}
